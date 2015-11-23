85b4e5c0139a174e69242a6fe7aed105e40e26b3
ichitomo <ichitomo0421@gmail.com>
2015-11-24 05:55:46 +0900

    server側の処理を流し込む

--- LeapSv/main.cpp
+++ LeapSv/main.cpp
@@ -10,6 +10,17 @@
 #include <string.h>
 #include "../leapSDK/include/Leap.h"
 
+//ソケット通信
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
 using namespace Leap;
 
 class SampleListener : public Listener {
@@ -31,6 +42,12 @@ private:
 const std::string fingerNames[] = {"Thumb", "Index", "Middle", "Ring", "Pinky"};
 const std::string boneNames[] = {"Metacarpal", "Proximal", "Middle", "Distal"};
 const std::string stateNames[] = {"STATE_INVALID", "STATE_START", "STATE_UPDATE", "STATE_END"};
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
 
 void SampleListener::onInit(const Controller& controller) {
     std::cout << "Initialized" << std::endl;
@@ -54,6 +71,15 @@ void SampleListener::onExit(const Controller& controller) {
 }
 
 void SampleListener::onFrame(const Controller& controller) {
+    //socket
+    
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    
+    
     // Get the most recent frame and report some basic information
     const Frame frame = controller.frame();
     std::cout << "Frame id: " << frame.id()
@@ -186,6 +212,35 @@ void SampleListener::onFrame(const Controller& controller) {
     if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
         std::cout << std::endl;
     }
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);//ソケットの生成
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi("9999");
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding"); //socketの登録
+    listen(sockfd,5);//ソケット接続準備
+    clilen = sizeof(cli_addr);
+    while(1) {
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *) &cli_addr,
+                           &clilen);//socketの接続待機（接続要求）
+        if (newsockfd < 0)
+            error("ERROR on accept");
+        bzero(buffer,256);
+        n = read(newsockfd,buffer,255);
+        if (n < 0) error("ERROR reading from socket");
+        printf("Here is the message: %s\n",buffer);
+        n = write(newsockfd,"I got your message",18);
+        if (n < 0) error("ERROR writing to socket");
+        close(newsockfd);
+    }
+    close(sockfd);//secket接続終わり
 }
 
 void SampleListener::onFocusGained(const Controller& controller) {
@@ -213,6 +268,8 @@ void SampleListener::onServiceConnect(const Controller& controller) {
 void SampleListener::onServiceDisconnect(const Controller& controller) {
     std::cout << "Service Disconnected" << std::endl;
 }
+
+
 
 int main(int argc, char** argv) {
     // Create a sample listener and controller
