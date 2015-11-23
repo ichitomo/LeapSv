39ceb24e2dd13de38e0cb15867e27540d0501bbe
ichitomo <ichitomo0421@gmail.com>
2015-11-24 04:40:44 +0900

    Initial Commit

---
+++ LeapSv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeapSv
+//
+//  Created by 一ノ瀬智太 on 2015/11/24.
+//  Copyright © 2015年 ichinose.tomohiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
